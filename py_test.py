0fa3ca31fa0a9e4b5b1e50d368589c03afdbb987
grwnek <93273178+grwnek@users.noreply.github.com>
2022-02-15 14:27:17 -0500

    Add files via upload

---
+++ py_test.py
@@ -0,0 +1,35 @@
+from errno import errorcode
+import mysql.connector
+from  mysql.connector import errorcode
+
+config = {
+    "user": "root",
+    "password": "8734910K5@62jq",
+    "host": "localhost",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("   The supplied username or password are invalid")
+
+    elif err.errno == errorcode.BAD_DB_ERROR:
+        print("   The specified database does not exist")
+
+    else:
+        print(err)
+    
+finally:
+    db.close()
+
+
